208d401eb0732500e50262cab27334e9632c6c3b
STEVE NEPOTE <senepote@gmail.com>
2018-06-16 21:36:37 -0500

    Inital Setup

---
+++ js/app.js
@@ -0,0 +1,41 @@
+console.log('js is linked yo');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//FISHER GATES SHUFFLE//
+
+function shuffle(array) {
+  var copy = [], n = array.length, i;
+
+  // While there remain elements to shuffle…
+  while (n) {
+
+    // Pick a remaining element…
+    i = Math.floor(Math.random() * array.length);
+
+    // If not already shuffled, move it to the new array.
+    if (i in array) {
+      copy.push(array[i]);
+      delete array[i];
+      n--;
+    }
+  }
+
+  return copy;
+}
