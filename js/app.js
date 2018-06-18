366583f9ab866078fa30044695fca5804366701e
STEVE NEPOTE <senepote@gmail.com>
2018-06-17 20:15:22 -0500

    functions and array build

--- js/app.js
+++ js/app.js
@@ -2,27 +2,30 @@ console.log('js is linked yo');
 
 
 //deck, cards, player, gameBoard
-var deck =
+var deck = new Array ();
 var cardNumbers = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
 var cardSuits = ["Spades", "Hearts", "Diamonds", "Clubs"];
 
 // cards
 
 $(()=> {
-  function cutDeck(){
-
-    deck =
-    for (var i = 0; i < values.length; i++)
-    {
-      for (var x = 0; x < suits.length; x++)
-    }
+  const createDeck()
+  {
+      deck = new Array();
+      for (var i = 0 ; i < cardNumbers.length; i++)
+      {
+          for(var x = 0; x < suits.length; x++)
+          {
+              var weight = parseInt(cardNumbers[i]);
+              if (cardNumbers[i] == "J" || cardNumbers[i] == "Q" || cardNumbers[i] == "K")
+                  weight = 10;
+              if (cardNumbers[i] == "A")
+                  weight = 11;
+              var card = { Value: cardNumbers[i],Suit: cardSuits[x], Weight: weight };
+              deck.push(card);
+          }
+      }
   }
-
-
-
-
-
-
 
 
 });
