db3621238429af8eff9a2479c3c2752ca25773fb
STEVE NEPOTE <senepote@gmail.com>
2018-06-20 21:56:35 -0500

    js debugged

--- Blackjack/js/app.js
+++ Blackjack/js/app.js
@@ -1,138 +1,53 @@
 //new build
 
-//buttons
-//dealButton hitButton stayButton
-
-//functions & game logic
-
-//if (player > 21) return player win = false;
-//if (dealer > 21) reutnr player win = true;
-//if dealer == 21 return text "Blackjack"
-(function (){
-  var cardDeck = new Deck();
-  var win = 0;
-  var lose = 0;
-})
-
-var winnerLoser = function (playersHand, dealerHand){
-    var result = '',
-        dealerScore = dealerHand.score(),
-        playerScore = playersHand.score();
-//if loops
-    if (playerScore  > 21 || dealerScore === 21){
-      result = "oh no! You've lost!!";
-      lose++;
-    }
-    else if (dealerScore > playerScore){
-      result = "oh no! You've lost!!";
-      lose++;
-    }
-    else if (dealerScore > 21 || playerScore === 21 || playerScore > dealerHand.score()){
-      result = "Woooooo!! You're a winner!!!";
-      win++
-    }
-    else if (dealerScore === playerScore){
-      result = "You tied!";
-    }
-
-    return result+
-var dealerHand = function (){
-  var hand = new dealerHand(deck);
-
-  while (hand.score() < 17){
-    hand.hitButton();
-  }
-  return hand;
-}
-
-
-
 //card build
 //testing js switch statement
 
 //alt card building
-var deck = new Array ();
+var deck = new Array();
 var cardNumber = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
 var cardSuit = ['♠', '♥', '♦', '♣'];
 var players = new Array();
 
-function createDeck()
-{
-    deck = new Array();
+const createDeck =() => {
     for (var i = 0 ; i < cardNumber.length; i++)
     {
-        for(var x = 0; x < suits.length; x++)
+        for(var x = 0; x < cardSuit.length; x++)
         {
             var weight = parseInt(cardNumber[i]);
             if (cardNumber[i] == "J" || cardNumber[i] == "Q" || cardNumber[i] == "K")
                 weight = 10;
             if (cardNumber[i] == "A")
                 weight = 11;
-            var cards = { Value: cardNumber[i],Suit: cardSuits[x], Weight: weight };
+            var cards = { Value: cardNumber[i],Suit: cardSuit[x], Weight: weight };
             deck.push(cards);
         }
     }
     return deck;
 }
+// call the deck
+createDeck()
+
+
 //the shuffle
 
-// function shuffle(){
-// 	for (var i = 0; i < 1000; i++)
-// 	{
-// 		var location1 = Math.floor((Math.random() * deck.length));
-// 		var location2 = Math.floor((Math.random() * deck.length));
-// 		var tmp = deck[location1];
-//
-// 		deck[location1] = deck[location2];
-// 		deck[location2] = tmp;
-// 	}
-// }
-
-//worked out diff way with Karolin
-
-function createCardDesigns(card)
-{
-var cardElement = document.createElement('div');
-var icon = '';
-if (card.Suit == 'Hearts')
-icon='♥';
-else if (card.Suit == 'Spades')
-icon = '♠';
-else if (card.Suit == 'Diamonds')
-icon = '♦';
-else
-icon = '♣';
-
-cardElement.className = 'card2';
-cardElement.innerHTML = card.Value + '' + icon;
-return cardElement;
-}
-
-function renderDeck()
-{
-	for(var i = 0; i < deck.length; i++)
+function shuffle(){
+	for (var i = 0; i < 1000; i++)
 	{
-		var card = document.createElement("div");
-		var num = document.createElement("div");
-		var suit = document.createElement("div");
-		card.className = "card";
-		num.className = "num";
-		suit.className = "suit " + deck[i].Suit;
+		var location1 = Math.floor((Math.random() * deck.length));
+		var location2 = Math.floor((Math.random() * deck.length));
+		var tmp = deck[location1];
 
-		num.innerHTML = deck[i].Value;
-		card.appendChild(num);
-		card.appendChild(suit);
-
-		document.getElementById("deck").appendChild(cards);
+		deck[location1] = deck[location2];
+		deck[location2] = tmp;
 	}
 }
 
-function load()
-{
-	deck = getDeck();
-	shuffle();
-	renderDeck();
-}
+//worked out diff way with Karolin
+//added elements to the Array
+//commented out
+
+//remove renderDeck
 
 
 var hand = function (deck){
@@ -158,19 +73,80 @@ var hand = function (deck){
 
 //players build
 
-var playerHand = function (){
+// var playerHand = function (){
+//
+// }
 
+
+//functions & game logic
+//deal 4 cards, 2 player 2 dealer
+
+//if (player > 21) return player win = false;
+//if (dealer > 21) reutnr player win = true;
+//if dealer == 21 return text "Blackjack"
+// (function (){
+  // var cardDeck = new Deck();
+  var win = 0;
+  var lose = 0;
+// })
+
+
+var checkWin = function (playersHand, dealerHand){
+    var result = '',
+        dealerScore = dealerHand.score(),
+        playerScore = playersHand.score();
+
+var winCode = checkWin(playerScore, dealerScore);
+//if loops
+//win and lose
+
+// check win for 21 or bust
+// once stay - no more cards
+
+    if (playerScore  > 21 || dealerScore === 21){
+      result = "oh no! You've lost!!";
+      //2;
+      lose++;
+    }
+    else if (dealerScore > playerScore){
+      result = "oh no! You've lost!!";
+      lose++;
+      //2;
+    }
+    else if (dealerScore > 21 || playerScore === 21 || playerScore > dealerHand.score()){
+      result = "Woooooo!! You're a winner!!!";
+      //remove
+      win++
+      //1;
+    }
+    else if (dealerScore === playerScore){
+      result = "You tied!";
+      //3;
+    }
+
+    return result;
+
+var dealerHand = function (){
+  var hand = new dealerHand(deck);
+
+  while (hand.score() < 17){
+    hand.hitButton();
+    checkWin()
+  }
+  return hand;
 }
 
 
 
+//buttons
+//dealButton hitButton stayButton
 
 
 
 $(() =>{
 
-var playerHand;
-var dealerHand;
+// var playerHand;
+// var dealerHand;
 
 var updateGame = function (){
   $playerHand.html(playerHand.toHTML());
@@ -225,6 +201,50 @@ $stay.on('click', function (){
 
 // graveyard
 
+// function renderDeck()
+// {
+// 	for(var i = 0; i < deck.length; i++)
+// 	{
+// 		var card = document.createElement("div");
+// 		var num = document.createElement("div");
+// 		var suit = document.createElement("div");
+// 		card.className = "card";
+// 		num.className = "num";
+// 		suit.className = "suit " + deck[i].Suit;
+//
+// 		num.innerHTML = deck[i].Value;
+// 		card.appendChild(num);
+// 		card.appendChild(suit);
+//
+// 		document.getElementById("deck").appendChild(cards);
+// 	}
+// }
+//
+// function load()
+// {
+// 	deck = getDeck();
+// 	shuffle(deck);
+// 	renderDeck();
+// }
+
+
+// function createCardDesigns(card)
+// {
+// var cardElement = document.createElement('div');
+// var icon = '';
+// if (card.Suit == 'Hearts')
+// icon='♥';
+// else if (card.Suit == 'Spades')
+// icon = '♠';
+// else if (card.Suit == 'Diamonds')
+// icon = '♦';
+// else
+// icon = '♣';
+//
+// cardElement.className = 'card2';
+// cardElement.innerHTML = card.Value + '' + icon;
+// return cardElement;
+// }
 
 // var deckOfCards = function (){
 //     var cards = [];
